abaa65660fc6b286ec3c074aaf1811dd77151a32
Iwan de Jong <idj.idejong@gmail.com>
2025-08-20 17:48:20 +0200

    for xxhash

--- deberta.sh
+++ deberta.sh
@@ -21,7 +21,7 @@ module purge
 module load chpc/python/anaconda/3-2021.11
 
 # xxhash
-export LD_LIBRARY_PATH=/apps/chpc/chem/anaconda3-2021.11/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/apps/chpc/chem/anaconda3-2021.11/pkgs/xxhash-0.8.0-h7f98852_3/lib:$LD_LIBRARY_PATH"
 
 # Activate venv
 source /mnt/lustre/users/idejong/venv/bin/activate
