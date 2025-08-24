38f2fc603550d6e5ce8967a76c45e9993580cb15
Iwan de Jong <idj.idejong@gmail.com>
2025-08-24 05:52:07 +0200

    a fixed `v_threshold` of 0.5

--- examples/spikelora_finetuning/best_params.py
+++ examples/spikelora_finetuning/best_params.py
@@ -19,9 +19,9 @@
 # }
 
 BEST_PARAMS = {
-  "cola": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.55}, # 4
-  "mrpc": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.55}, # 4
-  "stsb": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r':8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.55}, # 4
+  "cola": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.5}, # 4
+  "mrpc": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.5}, # 4
+  "stsb": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 20, 'lora_r':8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.5}, # 4
 
   "wnli": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 10, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.5}, # 4
   "sst2": {'learning_rate': 8e-4, 'batch_size': 32, 'num_epochs': 10, 'lora_r': 8, 'lora_alpha': 16, 'lora_dropout': 0.0, 'v_threshold': 0.5}, # 12
