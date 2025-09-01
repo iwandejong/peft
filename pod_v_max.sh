b037853d5f34da963f529f51dbc450fa5ae6f95b
Iwan de Jong <idj.idejong@gmail.com>
2025-09-01 19:31:56 +0200

    few cola runs

--- pod_v_max.sh
+++ pod_v_max.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for TASK in cola
 do
-for SEED in 1 2 3 4 5
+for SEED in 2 3 4 5
 do
-for V in 2.0 5.0
+for V in 2.0
 do
 echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --seed ${SEED} --v ${V}"
 rm -f ${TASK}_${SEED}_v${V}_output.log ${TASK}_${SEED}_v${V}_error.log
