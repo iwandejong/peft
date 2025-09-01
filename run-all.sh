d36a2cfbb2771052f9bef45d48c53cb518256798
Iwan de Jong <idj.idejong@gmail.com>
2025-09-01 22:21:37 +0200

    chpc runs

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,8 @@
-qsub -v TASK=cola,LORA=true deberta_4.sh -N cola-lora
-qsub -v TASK=mrpc,LORA=true deberta_4.sh -N mrpc-lora
-qsub -v TASK=stsb,LORA=true deberta_4.sh -N stsb-lora
-qsub -v TASK=rte,LORA=true deberta_4.sh -N rte-lora
-qsub -v TASK=sst2,LORA=true deberta_12.sh -N sst2-lora
-qsub -v TASK=qnli,LORA=true deberta_12.sh -N qnli-lora
-qsub -v TASK=qqp,LORA=true deberta_12.sh -N qqp-lora
-qsub -v TASK=mnli,LORA=true deberta_12.sh -N mnli-lora
+qsub -v TASK=cola,LORA=true,WANDB_PROJECT=chpc deberta_4.sh -N cola-lora
+qsub -v TASK=mrpc,LORA=true,WANDB_PROJECT=chpc deberta_4.sh -N mrpc-lora
+qsub -v TASK=stsb,LORA=true,WANDB_PROJECT=chpc deberta_4.sh -N stsb-lora
+qsub -v TASK=rte,LORA=true,WANDB_PROJECT=chpc deberta_4.sh -N rte-lora
+qsub -v TASK=sst2,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N sst2-lora
+qsub -v TASK=qnli,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N qnli-lora
+qsub -v TASK=qqp,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N qqp-lora
+qsub -v TASK=mnli,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N mnli-lora
