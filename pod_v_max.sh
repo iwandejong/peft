a10c9a367723aaf62e6f2c6a2bab333dc077b134
Iwan de Jong <idj.idejong@gmail.com>
2025-09-01 16:53:39 +0200

    add extreme v_thresholds

---
+++ pod_v_max.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for TASK in cola
+do
+for SEED in 1 2 3 4 5
+do
+for V in 2.0 5.0
+do
+echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --seed ${SEED} --v ${V}"
+rm -f ${TASK}_${SEED}_v${V}_output.log ${TASK}_${SEED}_v${V}_error.log
+python3 spikelora_finetuning/deberta_chpc.py --task "$TASK" --seed ${SEED} --v ${V} > ${TASK}_${SEED}_v${V}_output.log 2> ${TASK}_${SEED}_v${V}_error.log
+done
+done
+done
