f525327f24766056b14511783e3ee592a9587af4
Iwan de Jong <idj.idejong@gmail.com>
2025-09-03 17:31:36 +0200

    remove adalora for now (not tested)

--- pod_glue.sh
+++ pod_glue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for TASK in cola mrpc stsb rte sst2 qnli qqp mnli
 do
-for LORA in "" "--adalora" "--lora"
+for LORA in "" "--lora"
 do
 echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --rank $RANK $LORA"
 python3 spikelora_finetuning/deberta_chpc.py --task $TASK --rank $RANK $LORA > logs/${TASK}_r${RANK}${LORA//--/--}.log 2>&1
--- run-all.sh
+++ run-all.sh
@@ -7,11 +7,16 @@ qsub -v TASK=qnli,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N qnli-lora
 qsub -v TASK=qqp,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N qqp-lora
 qsub -v TASK=mnli,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N mnli-lora
 
-qsub -v TASK=cola,LORA=true,SEED=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
-qsub -v TASK=cola,LORA=true,SEED=2 deberta_4.sh -N cola-lora -l walltime=01:00:00
-qsub -v TASK=cola,LORA=true,SEED=3 deberta_4.sh -N cola-lora -l walltime=01:00:00
-qsub -v TASK=cola,LORA=true,SEED=4 deberta_4.sh -N cola-lora -l walltime=01:00:00
-qsub -v TASK=cola,LORA=true,SEED=5 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,LORA=true,SEED=1,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,LORA=true,SEED=2,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,LORA=true,SEED=3,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,LORA=true,SEED=4,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,LORA=true,SEED=5,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,SEED=1,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,SEED=2,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,SEED=3,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,SEED=4,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
+qsub -v TASK=cola,SEED=5,RANK=1 deberta_4.sh -N cola-lora -l walltime=01:00:00
 
 # sst2 (both lora=true and false)
 qsub -v TASK=sst2,LORA=true,SEED=1 deberta_4.sh -N sst2-lora -l walltime=4:00:00
