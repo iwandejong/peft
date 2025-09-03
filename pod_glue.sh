850cbb363fa234240f1b2a8a55965bd9e0abde34
Iwan de Jong <idj.idejong@gmail.com>
2025-09-03 17:32:14 +0200

    potential bug

--- pod_glue.sh
+++ pod_glue.sh
@@ -3,7 +3,7 @@ for TASK in cola mrpc stsb rte sst2 qnli qqp mnli
 do
 for LORA in "" "--lora"
 do
-echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --rank $RANK $LORA"
-python3 spikelora_finetuning/deberta_chpc.py --task $TASK --rank $RANK $LORA > logs/${TASK}_r${RANK}${LORA//--/--}.log 2>&1
+echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK $LORA"
+python3 spikelora_finetuning/deberta_chpc.py --task $TASK $LORA > logs/${TASK}_${LORA//--/--}.log 2>&1
 done
 done
