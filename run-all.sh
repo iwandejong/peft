7b4f7791b4530aede320ea0956ac5e537dd90be8
Iwan de Jong <idj.idejong@gmail.com>
2025-09-04 23:21:37 +0200

    new sst

--- run-all.sh
+++ run-all.sh
@@ -53,9 +53,9 @@ qsub -v TASK=mnli,LORA=true,WANDB_PROJECT=chpc deberta_12.sh -N mnli-lora
 
 # qsub -v TASK=cola,LORA=true,SEED=1,RANK=32 deberta_4.sh -N cola-lora-1-32 -l walltime=01:30:00 #
 # qsub -v TASK=cola,LORA=true,SEED=2,RANK=32 deberta_4.sh -N cola-lora-2-32 -l walltime=01:30:00 #
-qsub -v TASK=cola,LORA=true,SEED=3,RANK=32 deberta_4.sh -N cola-lora-3-32 -l walltime=01:30:00
-qsub -v TASK=cola,LORA=true,SEED=4,RANK=32 deberta_4.sh -N cola-lora-4-32 -l walltime=01:30:00
-qsub -v TASK=cola,LORA=true,SEED=5,RANK=32 deberta_4.sh -N cola-lora-5-32 -l walltime=01:30:00
+# qsub -v TASK=cola,LORA=true,SEED=3,RANK=32 deberta_4.sh -N cola-lora-3-32 -l walltime=01:30:00 #
+# qsub -v TASK=cola,LORA=true,SEED=4,RANK=32 deberta_4.sh -N cola-lora-4-32 -l walltime=01:30:00 #
+# qsub -v TASK=cola,LORA=true,SEED=5,RANK=32 deberta_4.sh -N cola-lora-5-32 -l walltime=01:30:00 #
 qsub -v TASK=cola,SEED=1,RANK=32 deberta_4.sh -N cola-spike-1-32 -l walltime=01:30:00
 qsub -v TASK=cola,SEED=2,RANK=32 deberta_4.sh -N cola-spike-2-32 -l walltime=01:30:00
 qsub -v TASK=cola,SEED=3,RANK=32 deberta_4.sh -N cola-spike-3-32 -l walltime=01:30:00
