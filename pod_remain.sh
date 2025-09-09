225a48bdb61acf305c8b03bc858962930f407e20
Iwan de Jong <idj.idejong@gmail.com>
2025-09-09 14:33:12 +0200

    final runs

--- pod_remain.sh
+++ pod_remain.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # MNLI, seed 5, spikelora
-python3 spikelora_finetuning/deberta_chpc.py --task mnli --seed 5 > r:mnli_5_output.log 2> r:mnli_5_error.log
+python3 spikelora_finetuning/deberta_chpc.py --task mnli --seed 5 > r_mnli_5_output.log 2> r_mnli_5_error.log
 
 # QQP, seed 5, lora
-python3 spikelora_finetuning/deberta_chpc.py --task qqp --seed 5 --lora > r:qqp_5_output.log 2> r:qqp_5_error.log
+python3 spikelora_finetuning/deberta_chpc.py --task qqp --seed 5 --lora > r_qqp_5_output.log 2> r_qqp_5_error.log
+
+# remove runpod
+sleep 60
+runpodctl remove pod $RUNPOD_POD_ID
