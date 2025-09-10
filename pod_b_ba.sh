248b9fa65009bea35ed5b338d4eb574b14302ea8
Iwan de Jong <idj.idejong@gmail.com>
2025-09-10 22:36:26 +0200

    don't kill the pod!

--- pod_b_ba.sh
+++ pod_b_ba.sh
@@ -4,6 +4,3 @@ do
 echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --project B-BA"
 python3 spikelora_finetuning/deberta_chpc.py --task $TASK --project B-BA > logs/${TASK}_BA.log 2>&1
 done
-
-sleep 60
-runpodctl remove pod $RUNPOD_POD_ID
