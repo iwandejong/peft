fde6273541b39544afc0a315c76647600d435342
Iwan de Jong <idj.idejong@gmail.com>
2025-09-11 11:40:57 +0200

    fix beyond script

--- pod_beyond.sh
+++ pod_beyond.sh
@@ -6,13 +6,10 @@ for LORA in "" "--lora"
 do
 for RANK in 64
 do
-for LR in 1e-4
-do
-for BZ in 8
+for LR in 5e-4
 do
 echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --r $RANK $LORA --project chpc --lr $LR --bz $BZ"
 python3 spikelora_finetuning/deberta_chpc.py --task $TASK --r $RANK $LORA --project chpc --lr $LR --bz $BZ > logs/${TASK}_r${RANK}_lr${LR}_bz${BZ}${LORA//--/--}.log 2>&1
-done
 done
 done
 done
@@ -25,12 +22,8 @@ for LORA in "" "--lora"
 do
 for LR in 1.1e-3
 do
-for BZ in 8
-do
 echo "Running: python3 spikelora_finetuning/deberta_chpc.py --task $TASK --r $RANK $LORA --project lrs --lr $LR --bz $BZ"
 python3 spikelora_finetuning/deberta_chpc.py --task $TASK --r $RANK $LORA --project lrs --lr $LR --bz $BZ > logs/${TASK}_r${RANK}_lr${LR}_bz${BZ}${LORA//--/--}.log 2>&1
-done
-done
 done
 done
 done
