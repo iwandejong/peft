d3d64878a44dfc0a93ddf37c435d6a7cd3e05e12
Iwan de Jong <idj.idejong@gmail.com>
2025-09-11 11:56:32 +0200

    add performance checking as well

--- pod_beyond.sh
+++ pod_beyond.sh
@@ -27,3 +27,13 @@ python3 spikelora_finetuning/deberta_chpc.py --task $TASK --r $RANK $LORA --proj
 done
 done
 done
+
+# performance
+for TASK in cola
+do
+for LORA in "" "--lora"
+do
+echo "Running: python3 spikelora_finetuning/performance.py --task $TASK --project performance $LORA"
+python3 spikelora_finetuning/performance.py --task $TASK --project performance $LORA > logs/performance_${TASK}_${LORA//--/--}.log 2>&1
+done
+done
