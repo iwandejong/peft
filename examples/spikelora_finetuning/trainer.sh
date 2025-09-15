eb5affaf88c9094babbdfc7721a1a9fe7a90aa1a
Iwan de Jong <idj.idejong@gmail.com>
2025-09-15 22:25:19 +0200

    use spikelora

--- examples/spikelora_finetuning/trainer.sh
+++ examples/spikelora_finetuning/trainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for QUANTIZE in "" "--quantize"
 do
-for LORA in "" "--lora"
+for LORA in "" "--use_spikelora"
 do
 echo "Running: python3 examples/spikelora_finetuning/spikelora_alpaca.py $QUANTIZE $LORA"
 python3 examples/spikelora_finetuning/spikelora_alpaca.py $QUANTIZE $LORA
