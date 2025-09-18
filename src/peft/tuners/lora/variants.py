ddab0b39ceac74d72cdb1f25e502adaf181cd872
Iwan de Jong <idj.idejong@gmail.com>
2025-09-18 23:12:51 +0200

    fix

--- src/peft/tuners/lora/variants.py
+++ src/peft/tuners/lora/variants.py
@@ -373,7 +373,7 @@ class SpikeLoraLinearVariant(LoraVariant):
         lora_out = lora_out.reshape(bs * sl, module.r[active_adapter]).to_sparse()
 
         with autocast('cuda', enabled=False, dtype=torch.float32):
-            up_proj = torch.sparse.mm(lora_out, lora_B.T)
+            up_proj = torch.sparse.mm(lora_out, lora_B.weight.T)
         up_proj = up_proj.reshape(bs, sl, dout)
         up_proj = up_proj.to(x.dtype)
 
