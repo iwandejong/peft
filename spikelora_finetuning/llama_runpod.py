6161f6641c06cbb2453dd259d2ce2dcf2cc68307
Iwan de Jong <idj.idejong@gmail.com>
2025-11-28 20:56:21 +0200

    only in 8bit

--- spikelora_finetuning/llama_runpod.py
+++ spikelora_finetuning/llama_runpod.py
@@ -147,11 +147,12 @@ def train_and_eval(**params) -> float:
     from transformers import BitsAndBytesConfig
 
     bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        # bnb_4bit_quant_type="nf4",
-        bnb_4bit_quant_type="fp4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16,
+        # load_in_4bit=True,
+        # # bnb_4bit_quant_type="nf4",
+        # bnb_4bit_quant_type="fp4",
+        # bnb_4bit_use_double_quant=True,
+        # bnb_4bit_compute_dtype=torch.bfloat16,
+        load_in_8bit=True
     )
 
     model = AutoModelForSequenceClassification.from_pretrained(
