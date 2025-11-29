11a210bf9f55d69f0b81e08b5db38229be5779aa
Iwan de Jong <idj.idejong@gmail.com>
2025-11-29 08:30:39 +0200

    token

--- spikelora_finetuning/lora_inference.py
+++ spikelora_finetuning/lora_inference.py
@@ -62,6 +62,7 @@ start_total = time.time()
 with torch.no_grad():
     for batch_texts in batchify(val_texts, BATCH_SIZE):
         inputs = tokenizer(batch_texts, padding=True, truncation=True, max_length=256, return_tensors="pt")
+        if "pad_token_id" not in inputs: inputs["pad_token_id"] = tokenizer.pad_token_id
         inputs = {k: v.to(DEVICE) for k, v in inputs.items()}
         outputs = model(**inputs)
         preds = torch.argmax(outputs.logits, dim=-1)
